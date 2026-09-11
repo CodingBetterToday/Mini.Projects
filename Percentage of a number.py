ebe5a7171191b131c51f952f53ec5076daec156b
Joao V. <coding.better.today@gmail.com>
2026-09-11 16:06:34 -0300

    Add files via upload

---
+++ Percentage of a number.py
@@ -0,0 +1,6 @@
+ValorInteiro = float(input("Insira o valor inteiro que você deseja calcular: "))
+PorcentagemACalcular = float(input("Insira a porcentagem do valor a ser calculado: "))
+
+Resultado = ValorInteiro * PorcentagemACalcular / 100
+
+print("O resultado é %r" % (Resultado))
