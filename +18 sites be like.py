4b4c4f500e9a21ec22756253f634a415b0277816
Joao V. <coding.better.today@gmail.com>
2026-09-11 16:21:13 -0300

    Add files via upload

---
+++ +18 sites be like.py
@@ -0,0 +1,4 @@
+X = int(input("Insira sua idade para o site de safadezas! "))
+if X < 18 : print("Pode não man.")
+elif X <= 99 : print("Se divirta.. he he he.")
+else : print("Você ainda consegue?")
